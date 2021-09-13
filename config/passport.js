d175d2f5691ee02ef4c5dff90e1749ca717da920
wojbiel833 <wojbiel833@gmail.com>
2021-09-13 21:04:41 +0200

    Split serv inhalt

---
+++ config/passport.js
@@ -0,0 +1,40 @@
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+
+const process = require("../nodemon.json");
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.clientID,
+      clientSecret: process.env.clientSecret,
+      callbackURL: process.env.callbackURL,
+    },
+    (accessToken, refreshToken, profile, done) => {
+      done(null, profile);
+      console.log(profile);
+    }
+  )
+);
+
+// serialize user when saving to session
+passport.serializeUser((user, serialize) => {
+  serialize(null, user);
+});
+
+// deserialize user when reading from session
+passport.deserializeUser((obj, deserialize) => {
+  deserialize(null, obj);
+});
+
+// Gdy zaimportujemy plik, to jeśli jest w nim jakiś "luźny" kod (a więc nieschowany w funkcji czy obiekcie), zostanie on automatycznie skompilowany. To prawie tak samo, jak przy ładowaniu plików JS w HTML-u za pomocą tagu <script>. Wystarczyło dodać odpowiedni tag z dobrą ścieżką do pliku i po wejściu na stronę, kod wewnątrz niego automatycznie się kompilował.
+
+// Node.js daje nam możliwość eksportowania funkcji czy obiektów i importowania ich potem pod odpowiednią nazwą, ale to tylko przydatna funkcjonalność, a nie wymóg.
+
+// Pojawia się więc pytanie, dlaczego w takim razie tak chętnie korzystamy z idei eksportów? Odpowiedź jest prosta. Daje nam to możliwość czytelnego przypisania jakichś funkcjonalności (lub danych) do stałych i wykorzystania ich potem w dalszym kodzie, w miejscu, gdzie jest to nam potrzebne.
+
+// Tymczasem w przypadku ładowania bez eksportu, wykonywanie tego "luźnego" kodu następuje od razu w miejscu, gdzie ten plik jest zaimportowany (więc w miejscu wywołania require). W większości sytuacji byłaby to ogromna wada, nie wspominając już o zwyczajnie gorszej czytelności.
+
+// Zauważ, że gdy ktoś wejdzie do naszego server.js, nie jest w stanie od razu stwierdzić, do czego w ogóle jest nam potrzebny plik config/passport. Można to sprawdzić dopiero po wejściu do tego pliku. Często wolimy postawić więc na większą przejrzystość.
+
+// Podsumowując, gdy używamy require wcale nie musimy korzystać z idei eksportów, lecz najczęściej będzie to zwyczajnie lepszym pomysłem – czytelniejszym i bardziej funkcjonalnym.
--- server.js
+++ server.js
@@ -2,45 +2,28 @@ const express = require("express");
 const cors = require("cors");
 const path = require("path");
 const hbs = require("express-handlebars");
-const passport = require("passport");
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const session = require("express-session");
+const passport = require("passport");
+
+const passportConfig = require("./config/passport");
 
 const app = express();
 
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID:
-        "529620112726-l88qbctib75jdmi6ooa9bql5ld01f89k.apps.googleusercontent.com",
-      clientSecret: "tQkBpP_g_lnSXJDysSFepUxR",
-      callbackURL: "http://localhost:8000/auth/google/callback",
-    },
-    (accessToken, refreshToken, profile, done) => {
-      done(null, profile);
-    }
-  )
-);
-
-// serialize user when saving to session
-passport.serializeUser((user, serialize) => {
-  serialize(null, user);
-});
-
-// deserialize user when reading from session
-passport.deserializeUser((obj, deserialize) => {
-  deserialize(null, obj);
-});
-
+// set handlebars as view engine
 app.engine(
   "hbs",
   hbs({ extname: "hbs", layoutsDir: "./layouts", defaultLayout: "main" })
 );
 app.set("view engine", ".hbs");
 
+// init session mechanism
 app.use(session({ secret: "secretString" }));
+
+// init passport
 app.use(passport.initialize());
 app.use(passport.session());
+
+// standard middleware
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -50,18 +33,8 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
-app.get(
-  "/auth/google",
-  passport.authenticate("google", { scope: ["email", "profile"] })
-);
-
-app.get("/user/logged", (req, res) => {
-  res.render("logged");
-});
-
-app.get("/user/no-permission", (req, res) => {
-  res.render("noPermission");
-});
+app.use("/auth", require("./routes/auth.routes"));
+app.use("/user", require("./routes/user.routes"));
 
 app.use("/", (req, res) => {
   res.status(404).render("notFound");
